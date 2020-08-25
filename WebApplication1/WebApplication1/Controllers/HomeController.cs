c7d702fabc928a32a9c41f1548552c255c515dc4
TeresaYang <s1022159@gm.pu.edu.tw>
2020-08-25 16:33:07 +0800

    新增Teresa 的一個Action

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -8,11 +8,6 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -24,6 +19,16 @@ namespace WebApplication1.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Teresa()
+        {
             return View();
         }
     }
