b84b2520608a9dfd93a9be56555132561b6bb796
Cheng Ching Jen <win50117@gmail.com>
2020-08-25 16:29:57 +0800

    我推

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -26,5 +26,10 @@ namespace WebApplication1.Controllers
 
             return View();
         }
+
+        public ActionResult ChingJen()
+        {
+            return View();
+        }
     }
 }
