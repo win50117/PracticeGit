703e8836d1fffd03178047ffc599f5eb88e423be
Cheng Ching Jen <win50117@gmail.com>
2020-10-13 23:50:43 +0800

    1

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -34,7 +34,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult Mamegoma()
         {
-            ViewBag.Message = "小海豹小海豹小海豹";
+            ViewBag.Message = "小海豹小海豹小海豹1";
 
             return View();
         }
